f237f437ffa26d0c4433836ad212b17f8434b67e
Tsuru <missigno@gmail.com>
2014-05-16 16:01:20 +0200

    Initial commit

---
+++ app/src/main/java/com/franz/indovinailnumero/app/Game.java
@@ -0,0 +1,243 @@
+package com.franz.indovinailnumero.app;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+public class Game extends ActionBarActivity {
+
+    public int fine, inizio, guess;
+    public int i=5;
+    public boolean finito=false;
+
+    MediaPlayer error,applausi,fail;
+
+
+    public void Inserisci(View view){
+        Button inserisci = (Button)findViewById(R.id.button);
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(inserisci.getWindowToken(), 0);
+
+        error=MediaPlayer.create(this,R.raw.error);
+        //error.setLooping(true);
+        applausi=MediaPlayer.create(this,R.raw.applausi);
+        //applausi.setLooping(true);
+        fail=MediaPlayer.create(this,R.raw.fail);
+        //fail.setLooping(true);
+
+        TextView inputText = (TextView)findViewById(R.id.editText3);
+        String guess1 =  inputText.getText()+"";
+        try {
+            int guess = Integer.parseInt(guess1);
+
+
+            TextView tentativi = (TextView) findViewById(R.id.textView2);
+            TextView alto_basso = (TextView) findViewById(R.id.textView3);
+            TextView vinto = (TextView) findViewById(R.id.textView4);
+            TextView numero = (TextView) findViewById(R.id.textView5);
+            EditText edit = (EditText) findViewById(R.id.editText3);
+
+            if (i > 0) {
+                if (guess == this.guess) {
+                    tentativi.setText("");
+                    alto_basso.setText("");
+
+                    vinto.setText("Hai\nVinto!!");
+                    //applausi.start();
+                    applausi.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mediaPlayer) {
+                            mediaPlayer.start();
+                        }
+                    });
+
+                    finito=true;
+
+                } else {
+                    edit.setText("");
+                    tentativi.setText("Ti sono rimasti ancora " + (i - 1) + " tentativi");
+                    if (guess < this.guess) {
+                        alto_basso.setText("Troppo basso!");
+                        i--;
+                        if(i>0)
+                            error.start();
+                    } else {
+                        alto_basso.setText("Troppo alto!");
+                        i--;
+                        if(i>0)
+                            error.start();
+                    }
+                }
+                if (i == 0) {
+                    tentativi.setText("");
+                    alto_basso.setText("");
+                    numero.setText("Il numero da indovinare era\n" + this.guess);
+                    vinto.setText("Hai\nPerso!!");
+                    fail.start();
+                    finito=true;
+
+
+
+                }
+            } /*else {
+                tentativi.setText("");
+                alto_basso.setText("");
+                numero.setText("Il numero da indovinare era\n" + guess);
+                vinto.setText("Hai\nPerso!!");
+                fail.start();
+                finito=true;
+
+            }*/
+        }catch(Exception e){
+            // 1. Instantiate an AlertDialog.Builder with its constructor
+            AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setMessage("Devi inserire un numero!")
+                    .setTitle("Errore!");
+
+            builder.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            dialogInterface.cancel();
+                        }
+                    }
+            );
+
+            // 3. Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+
+        }
+
+
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+        Intent intent=getIntent();
+
+        String f = intent.getStringExtra(MainActivity.FINE);
+        fine =Integer.parseInt(f);
+        String i = intent.getStringExtra(MainActivity.INIZIO);
+        inizio =Integer.parseInt(i);
+        Random random = new Random();
+        guess =random.nextInt(fine - inizio +1)+ inizio;
+
+        Log.d("guess", "Numero generato: " + guess);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.game, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify inizio parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void Ricomincia(View view){
+        final Intent intent= new Intent(this, MainActivity.class);
+        if(finito==false) {
+            // 1. Instantiate an AlertDialog.Builder with its constructor
+            AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setMessage("Vuoi ricominciare?")
+                    .setTitle("");
+
+            builder.setPositiveButton("Si",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            startActivity(intent);
+                        }
+                    }
+            );
+            builder.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    }
+            );
+            // 3. Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+
+            dialog.show();
+
+        }else{
+
+            // 1. Instantiate an AlertDialog.Builder with its constructor
+            AlertDialog.Builder builder = new AlertDialog.Builder(Game.this);
+
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setMessage("Vuoi rigiocare?")
+                    .setTitle("Partita Conclusa");
+
+            builder.setPositiveButton("Si",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            setResult(1);
+                            finish();
+                        }
+                    }
+            );
+            builder.setNegativeButton("No",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            setResult(0);
+                            finish();
+                            // Abandon audio focus when playback complete
+                            dialogInterface.cancel();
+                        }
+                    }
+            );
+            // 3. Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+
+            dialog.show();
+        }
+
+    }
+
+}
+
---
+++ app/src/main/java/com/franz/indovinailnumero/app/MainActivity.java
@@ -0,0 +1,128 @@
+package com.franz.indovinailnumero.app;
+
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import static java.lang.Integer.parseInt;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String FINE = "com.franz.guessthenumber.app.inizio";
+    public final static String INIZIO = "com.franz.guessthenumber.app.fine";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify inizio parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == 0) {
+            finish();
+        }
+    }
+
+    public class RemoteControlReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (Intent.ACTION_MEDIA_BUTTON.equals(intent.getAction())) {
+                KeyEvent event = (KeyEvent)intent.getParcelableExtra(Intent.EXTRA_KEY_EVENT);
+                if (KeyEvent.KEYCODE_MEDIA_PLAY == event.getKeyCode()) {
+                    // Handle key press.
+                }
+            }
+        }
+    }
+
+    public void StartGame(View view) {
+
+        try{
+            TextView inputText2 = (TextView)findViewById(R.id.editText2);
+            String fine =  inputText2.getText()+"";
+            TextView inputText=(TextView)findViewById(R.id.editText);
+            String inizio = inputText.getText()+"";
+            int a = parseInt(inizio);
+            int b = parseInt(fine);
+            if(a>b){
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage("'L vuoi capì che l'inizio dev'esse più piccolo della fine?")
+                        .setTitle("Sei tonto!");
+
+                builder.setPositiveButton("C'hai ragione",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.cancel();
+                            }
+                        }
+                );
+                // 3. Get the AlertDialog from create()
+                AlertDialog dialog = builder.create();
+
+                dialog.show();
+            }
+            else {
+                Intent intent = new Intent(MainActivity.this, Game.class);
+                intent.putExtra(FINE, fine);
+                intent.putExtra(INIZIO, inizio);
+                MainActivity.this.startActivityForResult(intent, 0);
+            }
+        }catch(Exception e){
+            // 1. Instantiate an AlertDialog.Builder with its constructor
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setMessage("Devi inserire dei numeri come parametro!")
+                    .setTitle("Errore!");
+            builder.setPositiveButton("Ok",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                        }
+                    }
+            );
+            // 3. Get the AlertDialog from create()
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+    }
+}
