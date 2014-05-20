7758bf7e7baeef0ce81d903dcb47022b6a88ce45
Tsuru <missigno@gmail.com>
2014-05-20 03:49:36 +0200

    -Modalità cerchio -Layout a livelli -Algoritmo da fare -.-

--- app/src/main/java/com/franz/indovinailnumero/app/CustomView.java
+++ app/src/main/java/com/franz/indovinailnumero/app/CustomView.java
@@ -6,7 +6,9 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import com.franz.indovinailnumero.app.R;
@@ -18,53 +20,93 @@ import java.util.Random;
  */
 public class CustomView extends View {
 
-    Bitmap mBmp;
-    Random mRnd;
-    Paint mPaint;
 
+    RectF rect;
     int w,h,bw,bh;
+    float iGreen;
+    float eGreen;
+    float iRed;
+    float eRed;
     int px=-1,py=-1;
+    float c= (float) 1.05;
+    Paint paint1 ;
+    Paint paint2 ;
+    Paint paint3 ;
+    Paint paint4;
+    Paint paint5;
+    float mRadius;
+    float anello;
     public CustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        iGreen = 0;
+        eGreen= 360;
+        iRed=0;
+        eRed=0;
+        paint1 = new Paint();
+        paint2 = new Paint();
+        rect = new RectF();
 
-        mBmp= BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher);      //carichiamo l'immagine in una bitmap
-        bw=mBmp.getWidth(); //larghezza bitmap
-        bh=mBmp.getHeight();//altezza
-        mPaint=new Paint(); // pennello
-        mPaint.setColor(Color.CYAN);
-        mPaint.setAntiAlias(true);
-        mRnd=new Random();
+
+        //Example values
+        paint1.setColor(Color.GREEN);
+        paint1.setStrokeCap(Paint.Cap.BUTT);
+        paint1.setStyle(Paint.Style.STROKE);
+        paint1.setAntiAlias(true);
+        paint2.setColor(Color.RED);
+        paint2.setAntiAlias(true);
+        paint2.setStrokeCap(Paint.Cap.BUTT);
+        paint2.setStyle(Paint.Style.STROKE);
 
     }
 
 
     @Override
     protected void onDraw(Canvas canvas) {
-        // TODO Auto-generated method stub
         super.onDraw(canvas);
+        //Log.d("On Draw ","h= "+h+" w= "+w);
+        //Roba magica, non toccare! //Modificare c per ogni evenienza
+        mRadius = (float) (c * (w/2.7));
+        anello= (float) (c * (mRadius/2 - w/10.8));
+        //Log.d("On Draw","mRadius: "+mRadius+" anello: "+anello);
+       // Log.d("On Draw","iGreen: "+iGreen+" eGreen: "+eGreen+ " iRed: "+iRed+" eRed: "+eRed);
+        paint1.setStrokeWidth(anello); //Larghezza tratto
+        paint2.setStrokeWidth(anello);
 
 
-        if(px==-1&&py==-1){ // se non abbiamo ancora settato le coordinate, posizioniamo la bmp al centro
-            px=w/2-bw/2; //metà della larghezza view, meno metà della figura
-            py=h/2-bh/2; //metà dell'altezza view, meno metà della figura
-        }
+        rect.set(canvas.getWidth()/2- mRadius, canvas.getHeight()/2 - mRadius, canvas.getWidth()/2 + mRadius, canvas.getHeight()/2 + mRadius);
+        canvas.drawArc(rect, iGreen, eGreen, false, paint1);
+        canvas.drawArc(rect, iRed, eRed, false, paint2);
+        /*
+        canvas.drawArc(rect, 120, 60, false, paint1);
+        canvas.drawArc(rect, 180, 60, false, paint2);
+        canvas.drawArc(rect, 240, 60, false, paint1);
+        canvas.drawArc(rect, 300, 60, false, paint2);*/
 
-
-        canvas.drawCircle(px+(bw/2), py+(bh/2), 70, mPaint); //disegnamo un cerchio con centro al centro della bitmap
-        canvas.drawBitmap( // disegnamo la bitmap
-                mBmp,
-                px,
-                py,
-                null);
-
+/* Disegnare DENTRO il cerchio
+paint4.setColor(Color.BLACK);
+canvas.drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, mRadius/2, paint4);
+paint5.setColor(Color.YELLOW);
+paint5.setStrokeWidth(3);
+paint5.setAntiAlias(true);
+paint5.setStrokeCap(Paint.Cap.BUTT);
+paint5.setStyle(Paint.Style.STROKE);
+canvas.drawCircle(canvas.getWidth()/2, canvas.getHeight()/2, mRadius/2, paint5);
+*/
 
     }
 
 
-    public void updatePosition(){
-        //posizione random x,y della bitmap all interno della view
-        px=mRnd.nextInt(w-bw);
-        py=mRnd.nextInt(h-bh);
+    public void updatePosition(boolean alto, int ins, int range) {
+        float n = (float) (ins * (360.0 /(float) range));
+        Log.d("update: ", "n= " + n);
+        if(alto){
+            iRed =n;
+            eRed = n;
+        }else{
+
+        }
+            //eGreen = iRed;
+        Log.d("On Draw","iGreen: "+iGreen+" eGreen: "+eGreen+ " iRed: "+iRed+" eRed: "+eRed);
         invalidate();
     }
 
